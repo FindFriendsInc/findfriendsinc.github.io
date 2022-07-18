03a7df4a27a9b4f9581d3f849431ba3f4382a42d
Sterling Tamboline <stamboli@sfu.ca>
2022-07-18 16:44:25 -0700

    Change Nav links

--- friendrr/src/Components/Navigationbar/Navigationbar.js
+++ friendrr/src/Components/Navigationbar/Navigationbar.js
@@ -7,13 +7,13 @@ const Navigationbar = () => {
 
     return (
         <Navbar id="nav-bar" className="p-3" collapseOnSelect expand="lg" bg="light" variant="light">
-            <Navbar.Brand href="/" className="mx-4">FindFriendsInc</Navbar.Brand>
+            <Navbar.Brand href="/#/" className="mx-4">FindFriendsInc</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" className="mx-4"/>
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end mx-4">
             <Nav>
-                <Nav.Link href="/about" className="mx-3">About</Nav.Link>
-                <Nav.Link href="/documentation" className="mx-3">Documentation</Nav.Link>
-                <Nav.Link href="/demo" className="mx-3">Demo</Nav.Link>
+                <Nav.Link href="/#/about" className="mx-3">About</Nav.Link>
+                <Nav.Link href="/#/documentation" className="mx-3">Documentation</Nav.Link>
+                <Nav.Link href="/#/demo" className="mx-3">Demo</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Navbar>
