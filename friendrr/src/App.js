385f0d10b5632fac3e701724a232d310bab37f99
Sterling Tamboline <stamboli@sfu.ca>
2022-07-18 14:28:07 -0700

    Set up routing and other pages

--- friendrr/src/App.js
+++ friendrr/src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navigationbar from './Components/Navigationbar/Navigationbar';
-import Homepage from './Pages/Homepage';
+import Aboutpage from "./Pages/Aboutpage/Aboutpage";
+import Demopage from "./Pages/Demopage/Demopage";
+import Docspage from "./Pages/Docspage/Docspage";
+import Homepage from './Pages/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Navigationbar />
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="findfriendsinc.github.io/" element={<Homepage/>}></Route>
+          <Route path="findfriendsinc.github.io/about" element={<Aboutpage/>}></Route>
+          <Route path="findfriendsinc.github.io/documentation" element={<Docspage/>}></Route>
+          <Route path="findfriendsinc.github.io/demo" element={<Demopage/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
