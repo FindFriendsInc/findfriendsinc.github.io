8edabe718dcb948c07a9fb0e9ffcfe2bae0cb7a3
Sterling Tamboline <stamboli@sfu.ca>
2022-07-18 15:27:04 -0700

    Switched to HashRouter

--- friendrr/src/App.js
+++ friendrr/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navigationbar from './Components/Navigationbar/Navigationbar';
 import Aboutpage from "./Pages/Aboutpage/Aboutpage";
@@ -10,15 +10,14 @@ function App() {
   return (
     <div className="App">
       <Navigationbar />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Homepage/>}></Route>
           <Route path="/about" element={<Aboutpage/>}></Route>
           <Route path="/documentation" element={<Docspage/>}></Route>
           <Route path="/demo" element={<Demopage/>}></Route>
         </Routes>
-      </BrowserRouter>
-      
+      </HashRouter>
     </div>
   );
 }
